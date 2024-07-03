91285e5c36791d173c9b5969a5d9741fd434a528
Mohaiminul375 <mohaiminul375@gmail.com>
2024-07-03 15:51:57 +0600

    add: create about section

---
+++ src/Pages/Home/About.jsx
@@ -0,0 +1,26 @@
+import { LuArrowUpRight } from 'react-icons/lu';
+import about_img from "../../assets/Rectangle 24.png"
+const About = () => {
+    return (
+        <section className="mt-40">
+            <div className="flex justify-between">
+                <div className='space-y-10 md:w-1/2 mr-32'>
+                     <button className="border-2 border-[#020043] text-[#020043] p-3 rounded-full text-base">Who we are</button>
+                     <h2 className="text-4xl font-bold text-[#020043]">We Help To Get <br /> Soultions</h2>
+                     <p className="text-[#4D4C7B] text-base">We are proud to be a trusted healthcare provider in our community, and we look forward to serving you and your family with excellence, integrity, and compassion. Your health is our priority, and we are here for you every step of the way. We believe in treating each patient with dignity, respect, and empathy, ensuring that they receive the attention and care they deserve.
+                     </p>
+                     <button className="flex items-center bg-[#FFC637] text-black font-bold p-2 rounded-md">Learn more <LuArrowUpRight/></button>
+                </div>
+                <div className='md:w-1/2 relative'>
+                    <img src={about_img} alt="" />
+                    <div className='bg-[#343268] text-white p-5 rounded-xl w-[395px] absolute -bottom-10 -left-14 '>
+                       <h2 className='text-2xl mb-5'>Our mission is simple</h2>
+                       <p className='text-lg'>To provide high-quality healthcare services that are accessible, personalized, and patient-centered.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default About;
