ea976e7f3b113b86644e1d54d042fcfb64b5d8f0
Mohaiminul375 <mohaiminul375@gmail.com>
2024-07-03 20:48:53 +0600

    add: create offer section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@
 import About from './About';
 import Banner from './Banner';
 import Faq from './Faq';
+import Offer from './Offer';
 import Services from './Services';
 import Testimonial from './Testimonial';
 
@@ -12,6 +13,7 @@ const Home = () => {
             <About></About>
             <Services></Services>
             <Faq></Faq>
+            <Offer></Offer>
             <Testimonial></Testimonial>
         </div>
     );
---
+++ src/Pages/Home/Offer.jsx
@@ -0,0 +1,46 @@
+import { LuArrowUpRight } from "react-icons/lu";
+import banner_offer from "../../assets/Rectangle 32.png";
+import logo from "../../assets/logo light.png";
+const Offer = () => {
+  return (
+    <section className="mt-40">
+      <div
+        style={{
+          backgroundImage: `
+      radial-gradient(347.71% 142.52% at 20% 78%, #020043, #0200431A 50%),
+      linear-gradient(200.57deg, #00C19D00 -90.412%, #02004333 100.111%),
+      url(${banner_offer})
+    `,
+        }}
+        className="h-[60vh] bg-cover bg-center bg-no-repeat mt-10 rounded-[48px]"
+      >
+        <div className="h-[60vh] relative">
+          <img
+            className="right-[45%] md:right-20 top-10 absolute"
+            src={logo}
+            alt=""
+          />
+          <div className="flex justify-center items-center text-center md:text-left md:justify-start md:items-center h-[60vh] md:ml-10 mt-5 md:mt-0">
+            <div className="text-white">
+              <h2 className="text-4xl font-bold mb-10">
+                Get Your <br />
+                First Appointment <br />
+                at 50% Off!
+              </h2>
+              <div className="flex items-center gap-3 ">
+                <button className="flex items-center bg-[#FFC637] text-black font-bold p-2 rounded-md">
+                  Appointment <LuArrowUpRight />
+                </button>
+                <button className="flex border-2 border-white items-center text-white font-bold p-2 rounded-md">
+                  Learn More <LuArrowUpRight />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Offer;
